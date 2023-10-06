54a82bdc9cd20becf1de6eeaf7dcb1c480f1bc0f
kkps-d <freakykaung@gmail.com>
2023-10-05 19:01:13 -0700

    Added initial source code files

---
+++ Win32Helper/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Win32Helper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
